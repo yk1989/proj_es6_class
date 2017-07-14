62bc87f5b6b79daeb1203365532fcffda251ca50
ProgramacionWeb <ProgramacionWeb@PC01>
2017-07-13 21:01:05 -0300

    update:

--- main.js
+++ main.js
@@ -23,13 +23,13 @@ class Cancion {
 nombre() {return this.titulo+""+this.artista}
 }
 
-var nuevaCancion= new Cancion("Wonder wall","Oasis","3.41","mp3");
+var nuevaCancion= new Cancion("Wonder wall","Oasis","3.41","mp3","mariachi");
 
 nuevaCancion.reproducir();
 nuevaCancion.nombre();
-console.log(nuevaCancion.nombre());
+console.log("la cancion se llama: "+nuevaCancion.nombre()+"El genero es:"+nuevaCancion.genero);
 
-var nuevaCancion2=new Cancion ("listen to our heart","some dude", "6.66","mp5");
+var nuevaCancion2=new Cancion ("listen to our heart","some dude", "6.66","mp5","rock");
 nuevaCancion2.reproducir();
 console.log("el estado de la cancion es: "+nuevaCancion2.estado+ "El genero es:"+nuevaCancion2.genero);
 console.log(nuevaCancion2.nombre());
